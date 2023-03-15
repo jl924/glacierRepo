3cf24946666bbb3117c728bd2d5934212d7e5427
Ermin Sljivo <esljivo1996@gmail.com>
2023-03-15 19:52:22 -0400

    create and implement Q&A component skeleton

---
+++ src/components/QuestionsAnswers/SearchQA.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// filter through the questions and answers section
+// remember placeholder text, container for input and align left
+const SearchQA = () => {
+
+  return (
+
+    <div className='text-center'>
+      <input className='search-field input input-bordered w-full max-w-xs' placeholder='Have a question? Search for answers...' />
+    </div>
+
+  );
+};
+
+export default SearchQA;
