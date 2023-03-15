446765d2b5e4ac9852a34b7dc68777c44f644541
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-03-15 19:21:51 -0400

    Add sass to our main repo

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,18 @@ module.exports = {
         include: path.resolve(__dirname, 'src'),
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+          'postcss-loader'
+        ],
+      },
     ]
   },
   plugins: [
