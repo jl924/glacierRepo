87d03baa27b47bd38ca21fc259b3c27b7f3afccd
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-03-15 19:51:23 -0400

    Flesh out an early skeleton for the ratings & reviews module

---
+++ src/components/RatingsReviews/ReviewListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReviewHeader from './ReviewHeader.js';
+
+const ReviewListItem = () => {
+  return (
+    <div>
+      <ReviewHeader />
+    </div>
+  )
+}
+
+export default ReviewListItem;
---
+++ src/components/RatingsReviews/UserDateInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserDateInfo = () => {
+  return (
+    <div className='userDateCheck '>
+      <span className='circle background-wh x10'><img className='check' src='https://uxwing.com/wp-content/themes/uxwing/download/checkmark-cross/check-mark-line-icon.png' /></span>
+      <a href='#'>
+        User1234, January 1, 2019
+      </a>
+    </div>
+  )
+}
+
+export default UserDateInfo;
