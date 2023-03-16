07aa2a03189f6ddfae6bc99fdc0d9b9b9a645017
Jae Lee <jaehlee.110994@gmail.com>
2023-03-16 16:19:34 -0400

    save some

--- src/components/Related/RelatedCard.js
+++ src/components/Related/RelatedCard.js
@@ -1,38 +1,15 @@
 import React from 'react';
-import RelatedCard from './Related/RelatedCard';
-import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 import {AiFillStar} from 'react-icons/Ai'
 
-const Related = () => {
 
 
+
+const RelatedCard = () => {
   return (
-    <div>
-    <div> Related Items </div>
-    <MdChevronLeft size={40}/>
-    <div className="carousel max-w-7xl p-4 space-x-2">
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-    </div>
-    <div> Your outfit </div>
-    <div className="carousel max-w-7xl p-4 space-x-4">
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-      <RelatedCard/>
-    </div>
     <div class="relative">
       <div className="carousel-item container w-[220px]">
     <div className="card w-[220px] card-bordered border-black">
-    <div className="tooltip tooltip" data-tip="helloooooooooooooooooooooooooooooo /n">
+    <div className="tooltip tooltip-open z-[99999]" data-tip="helloooooooooooooooooooooooooooooo" data-boundary="window" data-container="body">
     <button class="btn-sm absolute  top-1 right-1 bg-blue-500 text-white p-2 rounded hover:bg-blue-800">☆</button>
     </div>
   <figure><img class='w-full'src="https://nb.scene7.com/is/image/NB/mt21540ag_nb_70_i?$dw_detail_main_lg$&bgc=f1f1f1&layer=1&bgcolor=f1f1f1&blendMode=mult&scale=10&wid=1600&hei=1600" /></figure>
@@ -48,8 +25,9 @@ const Related = () => {
 </div>
 </div>
 </div>
-</div>
+
   );
 }
 
-export default Related;
+export default RelatedCard
+
