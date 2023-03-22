eadab844a5c0881563b175a549e8387e61c69ceb
Natale Toscano <46127615+Vorelli@users.noreply.github.com>
2023-03-21 21:31:56 -0400

    Update ratingsreview.test.js

--- src/tests/ratingsreview.test.js
+++ src/tests/ratingsreview.test.js
@@ -4,7 +4,7 @@ import RatingsReviews from "../components/RatingsReviews";
 import { render, screen } from "@testing-library/react";
 
 describe("RatingsReview", function () {
-  it("should render 5 reviews", () => {
+  it("should render 2 reviews", () => {
     const { queryAllByText, container } = render(<RatingsReviews />);
     expect(container.querySelectorAll(".review").length).toBe(2);
   });
