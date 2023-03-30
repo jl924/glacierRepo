eef807df36e8e470b60a5c820cf6ee5d86a265da
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-03-30 17:44:02 -0400

    Implement new support modal accessible from the new review modal

--- src/components/RatingsReviews/NewReviewModal.js
+++ src/components/RatingsReviews/NewReviewModal.js
@@ -7,6 +7,7 @@ export default function NewReviewModal({ checked, name, hidden }) {
   const dispatch = useDispatch();
   const { showMore } = useSelector((state) => state.ratingsReviewsReducer);
   const { isShown } = useSelector((state) => state.modalReducer.newReview);
+  console.log(isShown);
 
   return (
     <div
