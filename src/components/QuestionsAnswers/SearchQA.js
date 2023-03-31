4a66ca4d18220e5d55a704efaea110a40c4a7d2b
Ermin Sljivo <esljivo1996@gmail.com>
2023-03-31 10:50:08 -0400

    fix placeholder text theme

--- src/components/QuestionsAnswers/SearchQA.js
+++ src/components/QuestionsAnswers/SearchQA.js
@@ -8,7 +8,7 @@ import './QACss/QASearch.css';
 // remember placeholder text, container for input and align left
 const SearchQA = ({searchHandler}) => {
 
-  let inputCSS = 'searchBox search-field input input-bordered w-full rounded-none h-16 mr-3 py-3 input-primary';
+  let inputCSS = 'searchBox search-field input input-bordered w-full rounded-none h-16 mr-3 py-3 input-primary placeholder-secondary';
 
   return (
 
